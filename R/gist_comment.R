ef3c02f34e153bff1a69a333561995cb034ab5d9
Thomas Mock <j.thomasmock@gmail.com>
2022-05-12 15:15:47 -0500

    remove base R pipes - convert to %>%

--- R/gist_comment.R
+++ R/gist_comment.R
@@ -10,7 +10,7 @@
 #' @importFrom httr2 %>%
 #' @importFrom jsonlite toJSON
 #'
-gist_comment <- function(gist_id, comment){
+gist_comment <- function(gist_id="73b34a7d484360036fee122c5a745f44", comment="pbs.twimg.com/media/FBGfjADUYAUxiPz?format=png"){
 
   gist_error_body <- function(resp) {
     body <- httr2::resp_body_json(resp)
@@ -25,19 +25,19 @@ gist_comment <- function(gist_id, comment){
   # consider moving to token-based auth via httr2
   # token <- ifelse(!is.null(token), token, Sys.getenv("GITHUB_PAT"))
 
-  req_built <- "https://api.github.com" |>
-    request() |>
-    req_url_path_append("gists") |>
-    req_url_path_append(gist_id) |>
-    req_url_path_append("comments") |>
+  req_built <- "https://api.github.com" %>%
+    request() %>%
+    req_url_path_append("gists") %>%
+    req_url_path_append(gist_id) %>%
+    req_url_path_append("comments") %>%
     req_headers(
       gistr::gist_auth(),
       "User-Agent" = "gistr",
       Accept = "application/vnd.github.v3+json"
-    ) |>
+    ) %>%
     httr2::req_body_json(
       list(body = comment)
-    ) |>
+    ) %>%
     req_error(body = gist_error_body)
 
   httr2::req_perform(req_built)
